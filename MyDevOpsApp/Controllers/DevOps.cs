9bfd444cab8d2d42500012fbd807d232c5c16e86
kushwaharahul2012 <kushwaharahul2012@outlook.com>
2025-09-16 10:12:05 +0530

    some changes in Devops Controller

--- MyDevOpsApp/Controllers/DevOps.cs
+++ MyDevOpsApp/Controllers/DevOps.cs
@@ -1,6 +1,26 @@
-﻿namespace MyDevOpsApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyDevOpsApp.Controllers
 {
     public class DevOps
     {
+
+        public class MyBasicClass
+        {
+            // Constructor (optional)
+            public MyBasicClass()
+            {
+                // Initialization code if needed
+            }
+
+            // A method that performs an action but returns nothing
+            public void ShowMessage()
+            {
+                Console.WriteLine("Hello from MyBasicClass!");
+            }
+        }
+
+
+
     }
 }
